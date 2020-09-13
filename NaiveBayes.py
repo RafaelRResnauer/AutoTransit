c707084cec67878549049d8dee6c7b3782f5cd11
Rafael Ritzmann Resnauer <rafaritz@gmail.com>
2020-09-13 20:43:03 -0300

    Add files via upload

---
+++ NaiveBayes.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 13 15:35:01 2020
+
+@author: Rafael
+"""
+
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+csvfile = "D:\Rafael\Pendrive\Faculdade\10º semestre\TCC\Dataset_PRF_1_0_csv_600.csv"
+dataset = np.loadtxt(csvfile, delimiter=",")
+
+print(dataset.shape)
+
+x1=dataset[:,0:8]
+x2=dataset[:,10:24]
+X = np.concatenate((x1,x2),axis=1)
+y = dataset[:,9]
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.3, random_state=42, stratify=y)
+
+
+clfa = GaussianNB()
+
+clfa = clfa.fit(X_train, y_train)
+
+predicted=clfa.predict(X_test)
+
+score=clfa.score(X_test, y_test)
+
+matrix = confusion_matrix(y_test, predicted)
+
+print("Accuracy = %.2f " % score)
+print("Confusion Matrix:")
+print(matrix)
+
+clfb = GaussianNB()
+folds=10
+result = cross_val_score(clfb, X, y, cv=folds)
+print("\nCross Validation Results %d folds:" % folds)
+print("Mean Accuracy: %.2f" % result.mean())
+print("Mean Std: %.2f" % result.std())
+
+Z = cross_val_predict(clfb, X, y, cv=folds)
+cm=confusion_matrix(y, Z)
+print("Confusion Matrix:")
+print(cm)
+
---
+++ decision_tree.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 15:49:48 2020
+
+@author: Rafael
+"""
+
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import  cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+csvfile = "D:\Rafael\Pendrive\Faculdade\10º semestre\TCC\Dataset_PRF_1_0_csv_600.csv"
+dataset = np.loadtxt(csvfile, delimiter=",")
+
+print(dataset.shape)
+
+x1=dataset[:,0:8]
+x2=dataset[:,10:24]
+X = np.concatenate((x1,x2),axis=1)
+y = dataset[:,9]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.3, random_state=42, stratify=y)
+
+clfa = tree.DecisionTreeClassifier(criterion='entropy')
+clfa = clfa.fit(X_train, y_train)
+predicted=clfa.predict(X_test)
+
+score=clfa.score(X_test, y_test)
+
+
+matrix = confusion_matrix(y_test, predicted)
+
+
+print("Accuracy = %.2f " % score)
+print("Confusion Matrix:")
+print(matrix)
+
+
+clfb = tree.DecisionTreeClassifier(criterion='entropy')
+folds=10
+result = cross_val_score(clfb, X, y, cv=folds)
+print("\nCross Validation Results %d folds:" % folds)
+print("Mean Accuracy: %.2f" % result.mean())
+print("Mean Std: %.2f" % result.std())
+
+
+Z = cross_val_predict(clfb, X, y, cv=folds)
+cm=confusion_matrix(y, Z)
+print("Confusion Matrix:")
+print(cm)
+
+
+
+
+
