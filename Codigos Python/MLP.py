dfc335693af7de8b1297ffa14c4fcaa36210e3dd
Rafael Ritzmann Resnauer <rafaritz@gmail.com>
2020-11-08 11:54:09 -0300

    Parametros atualizados

--- Codigos Python/MLP.py
+++ Codigos Python/MLP.py
@@ -32,7 +32,7 @@ Testes usando Holdout
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.3, random_state=42, stratify=y)
 
 # Criacao do classificador
-clfa = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+clfa = MLPClassifier(activation = 'relu', alpha = 0.0001, hidden_layer_sizes= (10, 30, 10), learning_rate = 'adaptive', solver = 'adam', random_state=1)
 
 # Treinamento do classificador
 clfa = clfa.fit(X_train, y_train.values.ravel())
@@ -54,7 +54,7 @@ Testes usando Validacao Cruzada com 10 folds
 """
 
 # Criacao do classificador
-clfb = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+clfb = MLPClassifier(activation = 'relu', alpha = 0.0001, hidden_layer_sizes= (10, 30, 10), learning_rate = 'adaptive', solver = 'adam', random_state=1)
 
 # Treinamento do classificador
 folds=10
