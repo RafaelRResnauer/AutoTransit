6f426fdae703e4bdc6b8141a77c4124655d6efb7
Rafael Ritzmann Resnauer <rafaritz@gmail.com>
2020-11-08 11:55:32 -0300

    Update decision_tree.py

--- Codigos Python/decision_tree.py
+++ Codigos Python/decision_tree.py
@@ -32,7 +32,7 @@ Testes usando Holdout
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.3, random_state=42, stratify=y)
 
 # Criacao do classificador
-clfa = tree.DecisionTreeClassifier(criterion='entropy')
+clfa = tree.DecisionTreeClassifier(max_depth=10, min_samples_leaf=1, min_samples_split=30)
 
 # Treinamento do classificador
 clfa = clfa.fit(X_train, y_train)
@@ -53,7 +53,7 @@ print(matrix)
 Testes usando Validacao Cruzada com 10 folds
 """
 # Criacao do classificador
-clfb = tree.DecisionTreeClassifier(criterion='entropy')
+clfb = tree.DecisionTreeClassifier(max_depth=10, min_samples_leaf=1, min_samples_split=30)
 
 # Treinamento do classificador
 folds=10
